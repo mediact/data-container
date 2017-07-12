15dd0d6f522ba73e411537cad89fd678b5bc4e57
Ashoka de Wit <sjokki.dewit@mediact.nl>
2017-07-12 16:42:15 +0800

    Create trait to decorate a data container

---
+++ src/DataContainerDecoratorTrait.php
@@ -0,0 +1,169 @@
+<?php
+/**
+ * Copyright MediaCT. All rights reserved.
+ * https://www.mediact.nl
+ */
+
+namespace Mediact\DataContainer;
+
+/**
+ * Implements DataContainerInterface
+ */
+trait DataContainerDecoratorTrait
+{
+    /** @var DataContainerInterface */
+    private $storage;
+
+    /**
+     * Set data on a new storage.
+     *
+     * @param array $data
+     *
+     * @return void
+     */
+    protected function setData(array $data)
+    {
+        $this->storage = new DataContainer($data);
+    }
+
+    /**
+     * Set the storage.
+     *
+     * @param DataContainerInterface $storage
+     *
+     * @return void
+     */
+    protected function setStorage(DataContainerInterface $storage)
+    {
+        $this->storage = $storage;
+    }
+
+    /**
+     * Get the storage.
+     *
+     * @return DataContainerInterface
+     */
+    protected function getStorage(): DataContainerInterface
+    {
+        return $this->storage;
+    }
+
+    /**
+     * Check whether a path exists.
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function has(string $path): bool
+    {
+        return $this->getStorage()
+            ->has($path);
+    }
+
+    /**
+     * Get a value of a path.
+     *
+     * @param string $path
+     * @param mixed  $default
+     *
+     * @return mixed
+     */
+    public function get(string $path, $default = null)
+    {
+        return $this->getStorage()
+            ->get($path, $default);
+    }
+
+    /**
+     * Get the contained array.
+     *
+     * @return array
+     */
+    public function all(): array
+    {
+        return $this->getStorage()
+            ->all();
+    }
+
+    /**
+     * Set a value on a path.
+     *
+     * @param string $path
+     * @param mixed  $value
+     *
+     * @return void
+     */
+    public function set(string $path, $value = null)
+    {
+        $this->getStorage()
+            ->set($path, $value);
+    }
+
+    /**
+     * Remove a path if it exists.
+     *
+     * @param string $pattern
+     *
+     * @return void
+     */
+    public function remove(string $pattern)
+    {
+        $this->getStorage()
+            ->remove($pattern);
+    }
+
+    /**
+     * Find paths that match a pattern.
+     *
+     * @param string $pattern
+     *
+     * @return string[]
+     */
+    public function glob(string $pattern): array
+    {
+        return $this->getStorage()
+            ->glob($pattern);
+    }
+
+    /**
+     * Branch into a list of data containers.
+     *
+     * @param string $pattern
+     *
+     * @return DataContainerInterface[]
+     */
+    public function branch(string $pattern): array
+    {
+        return $this->getStorage()
+            ->branch($pattern);
+    }
+
+    /**
+     * Copy paths matching a pattern to another path.
+     *
+     * @param string $pattern
+     * @param string $destination
+     *
+     * @return void
+     */
+    public function copy(string $pattern, string $destination)
+    {
+        $this->getStorage()
+            ->copy($pattern, $destination);
+    }
+
+    /**
+     * Move paths matching a pattern to another path.
+     *
+     * @param string $pattern
+     * @param string $destination
+     *
+     * @return void
+     */
+    public function move(string $pattern, string $destination)
+    {
+        $this->getStorage()
+            ->move($pattern, $destination);
+    }
+}
